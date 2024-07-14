ee080d06083f85949f5af144d259a01e12663a3f
varun-11-maker <varunsinghal2003@gmail.com>
2024-07-14 18:17:43 +0530

    Designing home page

--- client/src/index.js
+++ client/src/index.js
@@ -1,19 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import {BrowserRouter} from 'react-router-dom'
-import { AuthProvider } from './context/auth';
-import 'antd/dist/reset.css'
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+import { AuthProvider } from "./context/auth";
+import { SearchProvider } from "./context/search";
+import "antd/dist/reset.css";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <AuthProvider>
-  <BrowserRouter>
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-  </BrowserRouter>
+    <SearchProvider>
+      <BrowserRouter>
+        <React.StrictMode>
+          <App />
+        </React.StrictMode>
+      </BrowserRouter>
+    </SearchProvider>
   </AuthProvider>
 );
 
