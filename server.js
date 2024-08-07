73e455cd76876480833ada75e615f13c17b2b90a
varun-11-maker <varunsinghal2003@gmail.com>
2024-08-07 23:08:33 +0530

    final commit

--- server.js
+++ server.js
@@ -8,6 +8,11 @@ import categoryRoutes from './routes/categoryRoutes.js'
 import productRoutes from './routes/productRoutes.js'
 import cors from "cors";
 import path from 'path'
+import { fileURLToPath } from 'url';
+
+// Define __filename and __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 dotenv.config()
 
 connectDB()
